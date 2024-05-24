8b79bb3225d0811e59a2b65ba5d8af3a4358515d
DiegoGTG <diegogomeztagle51@gmail.com>
2024-05-24 17:51:12 -0600

    Se arregla fallo en la busqueda por tipo,nombre y fecha

--- public_html/Estudios/buscar_estudios.php
+++ public_html/Estudios/buscar_estudios.php
@@ -1,12 +1,29 @@
 <?php
 // Conexión a la base de datos
 include '../includes/conexion.php';
+
 // Obtener el idExpediente y el término de búsqueda de la URL
 $idExpediente = $_GET["idExpediente"];
 $busqueda = $_GET["busqueda"];
 
-// Consulta SQL para buscar los estudios por nombre o fecha
-$sql = "SELECT e.idEstudios as idEstudio, tipoEstudio, nombreEstudio, descripcionEstudio, fechaEstudio FROM Estudios e INNER JOIN detalleEstudios de ON e.idEstudios = de.idEstudiosDE WHERE de.idExpedienteDE = $idExpediente AND (e.tipoEstudio LIKE '%$busqueda%' OR de.fechaEstudio = '$busqueda')";
+// Limpiar el término de búsqueda para evitar inyección SQL
+$busqueda = $conn->real_escape_string($busqueda);
+
+// Inicializar la condición de búsqueda
+$searchCondition = "e.tipoEstudio LIKE '%$busqueda%' OR e.nombreEstudio LIKE '%$busqueda%'";
+
+// Verificar si el término de búsqueda es una fecha válida
+if (DateTime::createFromFormat('Y-m-d', $busqueda) !== false) {
+    $searchCondition .= " OR de.fechaEstudio = '$busqueda'";
+}
+
+// Consulta SQL para buscar los estudios por tipo, nombre o fecha
+$sql = "SELECT e.idEstudios as idEstudio, tipoEstudio, nombreEstudio, descripcionEstudio, de.fechaEstudio 
+        FROM Estudios e 
+        INNER JOIN detalleEstudios de ON e.idEstudios = de.idEstudiosDE 
+        WHERE de.idExpedienteDE = $idExpediente 
+        AND ($searchCondition)";
+
 $result = $conn->query($sql);
 
 $estudios = array();
