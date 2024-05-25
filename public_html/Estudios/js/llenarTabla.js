0e97de775c656492142e47b74f7ce45173f9c629
DiegoGTG <diegogomeztagle51@gmail.com>
2024-05-24 19:15:34 -0600

    Se arregla un bug al modifiar los registros

--- public_html/Estudios/js/llenarTabla.js
+++ public_html/Estudios/js/llenarTabla.js
@@ -63,7 +63,8 @@ function buscar() {
                     </td>
                     <td>
                         <button type="button" class="btn btn-danger me-2" onclick="eliminarEstudio(${estudio.idEstudio}, ${estudio.idDetalleEstudio}, ${rowIndex})">Eliminar</button>
-                        <button type="button" class="btn btn-primary" onclick="modificarEstudio(this)">Modificar</button>
+                        <button type="button" class="btn btn-primary" onclick="modificarEstudio(${estudio.idEstudio},${rowIndex} )">Modificar</button>
+                    
                     </td>
                 `;
                 rowIndex++;
