80d7df7e94fc720ffcde429a050a552affc465a0
DiegoGTG <diegogomeztagle51@gmail.com>
2024-05-25 21:57:07 -0600

    Se agrega la opcion de Guardar citas

--- public_html/Citas/js/agregarCita.js
+++ public_html/Citas/js/agregarCita.js
@@ -1,2 +1,27 @@
-import { idPaciente, idExpediente, obtenerDatosSession } from '../../Estudios/js/obtenerExpediente.js';
 
+function guardarCita() {
+    var patientId = document.getElementById('input-IDpaciente').value;
+    var eventDate = document.getElementById('eventDate').value;
+    var eventTime = document.getElementById('eventTime').value;
+    var eventAttendance = document.getElementById('eventAttendance').checked ? 1 : 0;
+
+    $.ajax({
+        url: '../Calendario/guardar_cita.php',
+        type: 'POST',
+        data: {
+            patientId: patientId,
+            eventDate: eventDate,
+            eventTime: eventTime,
+            eventAttendance: eventAttendance
+        },
+        success: function(response) {
+            alert(response);
+            location.reload();
+            cargarCitas();
+            $('#modalAgregar').modal('hide');
+        },
+        error: function(error) {
+            alert('Error al guardar la cita: ' + error);
+        }
+    });
+}
